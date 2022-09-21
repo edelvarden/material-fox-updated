e493e3cb5c1752990ec19f28928f3e3543017127
edelvarden <edelvarden@gmail.com>
2022-09-21 17:39:05 +0300

    add tab icon overlay

--- user.js
+++ user.js
@@ -1,8 +1,7 @@
-/*** MaterialFox ***/
-/**  Mandatory	  **/
-user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true); // default is false
-user_pref("svg.context-properties.content.enabled", true); 				// default is false
-user_pref("layout.css.color-mix.enabled", true); 				        // default is false
+
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true); // for enable userChrome/userContent
+user_pref("svg.context-properties.content.enabled", true); 				// for svg
+user_pref("layout.css.color-mix.enabled", true); 				        // for color-mix
 
 /**  Recommended  (uncomment to apply) **/
 /* Replicate Chrome behaviour for clipped tabs */
